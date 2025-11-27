1a245b98a3d01b23af3ec322257df2b16fe08be0
Hounaida202 <etrichih@gmail.com>
2025-11-27 17:01:30 +0100

    Modification : modification lors de la classe UsertDTO

--- demo/src/main/java/com/example/demo/DTOs/Requests/UserDTO.java
+++ demo/src/main/java/com/example/demo/DTOs/Requests/UserDTO.java
@@ -1,6 +1,9 @@
 package com.example.demo.DTOs.Requests;
 
 import com.example.demo.enums.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,8 +11,13 @@ import lombok.Data;
 @Builder
 public class UserDTO {
 
+    @NotBlank(message="email est obligatoire")
+    @Email(message = "format d'email invalide")
     private String username;
+
+    @NotNull(message="role est obligatoire")
     private UserRole userRole;
+
     private String password;
 
 }
