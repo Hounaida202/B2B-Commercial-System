feeeb2a29fd24157a5c645f3b3ea92403711bc32
Hounaida202 <etrichih@gmail.com>
2025-11-27 14:20:32 +0100

    mettre en place la classe Client

--- demo/src/main/java/com/example/demo/entities/Client.java
+++ demo/src/main/java/com/example/demo/entities/Client.java
@@ -1,4 +1,30 @@
 package com.example.demo.entities;
 
+import com.example.demo.enums.CustomerTier;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="clients")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Client {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+
+    @Enumerated(EnumType.STRING)
+    private CustomerTier customerTier;
+
+    @OneToOne(cascade=CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user ;
+
 }
