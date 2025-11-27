8eda356be7965a92c4219eb77bcbb8afd52a8e76
Hounaida202 <etrichih@gmail.com>
2025-11-27 14:55:26 +0100

    dans le context de la gestion centralisé des exceptions , c'est la partie création d une classe errorException pour l'envoie d'un message d'erreur adabpté ,selon le cas

--- demo/src/main/java/com/example/demo/exceptions/ErrorException.java
+++ demo/src/main/java/com/example/demo/exceptions/ErrorException.java
@@ -1,4 +1,22 @@
 package com.example.demo.exceptions;
 
+import lombok.Getter;
+import java.time.LocalDateTime;
+
+@Getter
 public class ErrorException {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+
+    public ErrorException(String message, String error, int status, String path) {
+        this.timestamp = LocalDateTime.now();
+        this.message = message;
+        this.error = error;
+        this.status = status;
+        this.path = path;
+    }
 }
