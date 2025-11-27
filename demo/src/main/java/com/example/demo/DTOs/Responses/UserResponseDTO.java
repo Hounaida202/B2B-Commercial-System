5c1f14b8cc5afbb8959d8ca616e75918112536f8
Hounaida202 <etrichih@gmail.com>
2025-11-27 14:35:19 +0100

    la classe dto de UserResponse pour le compte user associé a un client lors de la recuperation des ses donnés

--- demo/src/main/java/com/example/demo/DTOs/Responses/UserResponseDTO.java
+++ demo/src/main/java/com/example/demo/DTOs/Responses/UserResponseDTO.java
@@ -1,4 +1,10 @@
 package com.example.demo.DTOs.Responses;
 
+import com.example.demo.enums.UserRole;
+import lombok.Data;
+
+@Data
 public class UserResponseDTO {
+    private String username ;
+    private UserRole role ;
 }
