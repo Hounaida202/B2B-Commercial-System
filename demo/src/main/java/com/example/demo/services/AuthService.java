5e34ff8a26faba0454594b4b21a25dad831485e9
Hounaida202 <etrichih@gmail.com>
2025-11-27 14:50:40 +0100

    SMR-7 La creation d'une classe service pour implementer la logique metier d'un systeme d authentification

--- demo/src/main/java/com/example/demo/services/AuthService.java
+++ demo/src/main/java/com/example/demo/services/AuthService.java
@@ -1,4 +1,25 @@
 package com.example.demo.services;
 
+import com.example.demo.DTOs.Requests.AuthRequestDTO;
+
+import com.example.demo.DTOs.Responses.AuthResponseDTO;
+
+import com.example.demo.entities.User;
+
+import com.example.demo.repositories.UserRepository;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+
+
+
 }
