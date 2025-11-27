b79f4dc9e98e8b4004912be3df870ada8899f965
Hounaida202 <etrichih@gmail.com>
2025-11-27 17:03:46 +0100

    Modification : modifications dans la classe client mapper pour conresspendre les noms entre l entity et le dto

--- demo/src/main/java/com/example/demo/mappers/ClientMapper.java
+++ demo/src/main/java/com/example/demo/mappers/ClientMapper.java
@@ -1,4 +1,15 @@
 package com.example.demo.mappers;
 
+import com.example.demo.DTOs.Requests.ClientDTO;
+import com.example.demo.DTOs.Requests.ProduitDTO;
+import com.example.demo.entities.Client;
+import com.example.demo.entities.Produit;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel="spring", uses= {UserMapper.class})
+
 public interface ClientMapper {
+
+    Client toEntity(ClientDTO clientDto);
+    ClientDTO toDTO(Client client);
 }
