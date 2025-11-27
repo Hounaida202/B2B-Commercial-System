0d6f1ebc583af6f296bd248d939a3c9be8af14fe
Hounaida202 <etrichih@gmail.com>
2025-11-27 14:25:42 +0100

    création de l'interface ClientRepository pour que hibernate pourra s'interagir avec la base depuis jpa

--- demo/src/main/java/com/example/demo/repositories/ClientRepository.java
+++ demo/src/main/java/com/example/demo/repositories/ClientRepository.java
@@ -1,4 +1,7 @@
 package com.example.demo.repositories;
 
-public interface ClientRepository {
+import com.example.demo.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client,Long> {
 }
