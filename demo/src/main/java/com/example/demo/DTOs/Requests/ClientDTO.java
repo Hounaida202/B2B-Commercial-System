3c7cb581ea35a56adbdc59fa1437b0ea78e77d6f
Hounaida202 <etrichih@gmail.com>
2025-11-27 23:05:44 +0100

    Modifications : modification lors de la classe dto client dto

--- demo/src/main/java/com/example/demo/DTOs/Requests/ClientDTO.java
+++ demo/src/main/java/com/example/demo/DTOs/Requests/ClientDTO.java
@@ -1,4 +1,21 @@
 package com.example.demo.DTOs.Requests;
 
+import com.example.demo.enums.CustomerTier;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+
+@Data
 public class ClientDTO {
+    @NotBlank(message="le nom est obligatoire")
+    private String nom;
+
+    @NotNull(message="le role est obligatoire")
+    private CustomerTier customerTier;
+
+    @NotNull(message="les infos email et password sont obligatoires")
+    @Valid
+    private UserDTO user;
 }
