ef59f65297d71eb07981420f5f79d941156a5d17
Hounaida202 <etrichih@gmail.com>
2025-11-27 17:03:01 +0100

    Modification : modifications dans la classe user mapper pour conresspendre les noms entre l entity et le dto

--- demo/src/main/java/com/example/demo/mappers/UserMapper.java
+++ demo/src/main/java/com/example/demo/mappers/UserMapper.java
@@ -1,4 +1,20 @@
 package com.example.demo.mappers;
 
+import com.example.demo.DTOs.Requests.UserDTO;
+import com.example.demo.entities.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.lang.annotation.Target;
+
+@Mapper(componentModel="spring")
+
 public interface UserMapper {
+
+    @Mapping(source="userRole" , target="role")
+
+    User toEntity(UserDTO userDto);
+
+    @Mapping(source="role" , target="userRole")
+    UserDTO toDTO(User user);
 }
