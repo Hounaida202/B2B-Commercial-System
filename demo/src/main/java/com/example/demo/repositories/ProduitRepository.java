666cedc547eb28efa491e9e4a1409073204f0923
Hounaida202 <etrichih@gmail.com>
2025-11-28 00:08:10 +0100

    SMR-20 Créations: creation des methodes jpa pour l'obtention des produits filtrés avec pagination

--- demo/src/main/java/com/example/demo/repositories/ProduitRepository.java
+++ demo/src/main/java/com/example/demo/repositories/ProduitRepository.java
@@ -2,7 +2,13 @@ package com.example.demo.repositories;
 
 import com.example.demo.entities.Produit;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 public interface ProduitRepository extends JpaRepository<Produit, Long> {
+
+    Page<Produit> findByNomContainingIgnoreCaseAndStock( String nom ,Integer stock , Pageable page);
+    Page<Produit> findByNomContainingIgnoreCase(String nom, Pageable pageable);
+
+    Page<Produit> findByStock(Integer stock, Pageable pageable);
 
 }
