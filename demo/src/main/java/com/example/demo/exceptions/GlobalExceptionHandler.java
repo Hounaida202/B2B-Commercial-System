3b46c0c7430874906be5acb1d42851b3031164a9
Hounaida202 <etrichih@gmail.com>
2025-11-27 15:24:33 +0100

    la creation de la classe global exception handler avec la methode de gestion des erreurs not found requests

--- demo/src/main/java/com/example/demo/exceptions/GlobalExceptionHandler.java
+++ demo/src/main/java/com/example/demo/exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,31 @@
 package com.example.demo.exceptions;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorException> handleNotFound(NotFoundException ex, WebRequest request) {
+
+        ErrorException err = new ErrorException(
+                ex.getMessage(),
+                "Not Found",
+                404,
+                request.getDescription(false)
+        );
+
+        return ResponseEntity.status(404).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidation(MethodArgumentNotValidException ex) {
+        String msg = ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+        return ResponseEntity.badRequest().body(msg);
+    }
+
 }
