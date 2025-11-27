c14e784393e6cdde12ba33164c4a4ddbafaad7c1
Hounaida202 <etrichih@gmail.com>
2025-11-27 14:30:32 +0100

    la création de la classe AuthResponseDTO dans la couche DTOs pour rendre une interaction indirecte avec l'entity lors de la recuperation des données sensibles des users depuis la base

--- demo/src/main/java/com/example/demo/DTOs/Responses/AuthResponseDTO.java
+++ demo/src/main/java/com/example/demo/DTOs/Responses/AuthResponseDTO.java
@@ -1,4 +1,15 @@
 package com.example.demo.DTOs.Responses;
 
+import com.example.demo.enums.CustomerTier;
+import com.example.demo.enums.UserRole;
+import lombok.Data;
+
+@Data
 public class AuthResponseDTO {
+    private String username ;
+    private UserRole role ;
+    private String msg ;
+
+
+
 }
