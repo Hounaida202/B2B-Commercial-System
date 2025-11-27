68b10462b760ed9e8c75767e9c91dbd5fea1a7de
Hounaida202 <etrichih@gmail.com>
2025-11-27 14:34:13 +0100

    une classe ClientResponse dto pour l envoie des donnés depuis la table users

--- demo/src/main/java/com/example/demo/DTOs/Responses/ClientResponseDTO.java
+++ demo/src/main/java/com/example/demo/DTOs/Responses/ClientResponseDTO.java
@@ -1,4 +1,16 @@
 package com.example.demo.DTOs.Responses;
 
+import com.example.demo.entities.User;
+import com.example.demo.enums.CustomerTier;
+import lombok.Data;
+
+@Data
 public class ClientResponseDTO {
+
+    private String nom;
+
+    private CustomerTier customerTier;
+
+    private UserResponseDTO user;
+
 }
