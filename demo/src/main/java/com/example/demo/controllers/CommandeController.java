7e1c9bb6d66e91ff2cfcc2adc4a218fa452cb864
Hounaida202 <etrichih@gmail.com>
2025-11-28 14:47:30 +0100

    SMR-27-ajout-commande-client Créations; creation de la classe controller de commande

--- demo/src/main/java/com/example/demo/controllers/CommandeController.java
+++ demo/src/main/java/com/example/demo/controllers/CommandeController.java
@@ -1,4 +1,14 @@
 package com.example.demo.controllers;
 
+
+import com.example.demo.services.CommandeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/commande")
 public class CommandeController {
+    @Autowired
+    private CommandeService commandeService;
 }
