a88f77905579cf7a34e23c1de1433dcca2270fb8
Hounaida202 <etrichih@gmail.com>
2025-11-28 16:38:21 +0100

    SMR-27-ajout-commande-client Modifications: modification dans le dto commande en remplacent attribut client par idclient

--- demo/src/main/java/com/example/demo/DTOs/Requests/CommandeRequestDTO.java
+++ demo/src/main/java/com/example/demo/DTOs/Requests/CommandeRequestDTO.java
@@ -1,4 +1,32 @@
 package com.example.demo.DTOs.Requests;
 
+import com.example.demo.enums.OrderStatus;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
 public class CommandeRequestDTO {
+
+    private List<Long> produitids;
+
+    private Long clientId;
+
+    private LocalDate date;
+
+    private Double sousTotal;
+
+    private Double remise;
+
+    private Double tva;
+
+    private Double total;
+
+    private String codePromo;
+
+    private OrderStatus orderStatus;
+
+    private Double montantRestant;
 }
