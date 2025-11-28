9164f1c314ac349ca5a900a346dc962caf9b1667
Hounaida202 <etrichih@gmail.com>
2025-11-28 16:41:27 +0100

    SMR-27-ajout-commande-client Créations: faire la methode creer commande dans la classe commande service

--- demo/src/main/java/com/example/demo/services/CommandeService.java
+++ demo/src/main/java/com/example/demo/services/CommandeService.java
@@ -1,10 +1,21 @@
 package com.example.demo.services;
 
 
+import com.example.demo.DTOs.Requests.CommandeRequestDTO;
+import com.example.demo.entities.Client;
+import com.example.demo.entities.Commande;
+import com.example.demo.entities.CommandeProduit;
+import com.example.demo.entities.Produit;
+import com.example.demo.exceptions.NotFoundException;
+import com.example.demo.mappers.CommandeMapper;
+import com.example.demo.repositories.ClientRepository;
 import com.example.demo.repositories.CommandeProduitRepository;
 import com.example.demo.repositories.CommandeRepository;
+import com.example.demo.repositories.ProduitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CommandeService {
@@ -12,5 +23,35 @@ public class CommandeService {
     private CommandeRepository commandeRepository;
     @Autowired
     private CommandeProduitRepository commandeProduitRepository;
+    @Autowired
+    private CommandeMapper commandeMapper;
+    @Autowired
+    private ProduitRepository produitRepository;
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public void creerCommande(CommandeRequestDTO commandeDto){
+
+                Commande commande = commandeMapper.toEntity(commandeDto);
+
+                Client client =clientRepository.findById(commandeDto.getClientId()).orElseThrow(
+                        ()->new NotFoundException("le client avec l id "+ commandeDto.getClientId()+ "n existe pas")
+                );
+                       commande.setClient(client);
+                for(Long pId: commandeDto.getProduitids()){
+                    Produit produit = produitRepository.findById(pId).orElseThrow(
+                            ()->new NotFoundException("produit avec l id "+ pId + "n existe pas"));
+
+                    CommandeProduit cp = new CommandeProduit();
+                    cp.setCommande(commande);
+                    cp.setProduit(produit);
+
+                    commande.getCommande_produit().add(cp);
+                }
+
+
+                commandeRepository.save(commande);
+
+    }
 
 }
