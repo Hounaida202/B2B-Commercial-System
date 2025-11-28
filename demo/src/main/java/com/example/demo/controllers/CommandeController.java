f88fba4b0c876dfbb35b5735248e0147f18742e8
Hounaida202 <etrichih@gmail.com>
2025-11-28 16:42:37 +0100

    SMR-27-ajout-commande-client Créations: l'ajout de l endpoint creerCommande dans le controller commande

--- demo/src/main/java/com/example/demo/controllers/CommandeController.java
+++ demo/src/main/java/com/example/demo/controllers/CommandeController.java
@@ -1,8 +1,12 @@
 package com.example.demo.controllers;
 
 
+import com.example.demo.DTOs.Requests.CommandeRequestDTO;
 import com.example.demo.services.CommandeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommandeController {
     @Autowired
     private CommandeService commandeService;
+
+    @PostMapping("creerCommande")
+    public ResponseEntity<?> creerCommande(@RequestBody CommandeRequestDTO dto){
+        commandeService.creerCommande(dto);
+
+        return ResponseEntity.status(201).body("bien");
+    }
+
 }
