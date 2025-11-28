a49692f9eff27935a7a2c7b7efffa2a954b74d7d
Hounaida202 <etrichih@gmail.com>
2025-11-28 12:26:24 +0100

    SMR-27-ajout-commande-client Créations: la classe service de Commande pour sa service metier

--- demo/src/main/java/com/example/demo/services/CommandeService.java
+++ demo/src/main/java/com/example/demo/services/CommandeService.java
@@ -1,4 +1,16 @@
 package com.example.demo.services;
 
+
+import com.example.demo.repositories.CommandeProduitRepository;
+import com.example.demo.repositories.CommandeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CommandeService {
+    @Autowired
+    private CommandeRepository commandeRepository;
+    @Autowired
+    private CommandeProduitRepository commandeProduitRepository;
+
 }
