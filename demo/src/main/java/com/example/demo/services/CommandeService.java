db9a44bb84e97dedc204f40e82e9196680b7c728
Hounaida202 <etrichih@gmail.com>
2025-12-01 21:43:51 +0100

    SMR-27-ajout-commande-clien Modifications : la creation de la methode creer commande pour la creation et le retourne d une commande

--- demo/src/main/java/com/example/demo/services/CommandeService.java
+++ demo/src/main/java/com/example/demo/services/CommandeService.java
@@ -2,12 +2,16 @@ package com.example.demo.services;
 
 
 import com.example.demo.DTOs.Requests.CommandeRequestDTO;
+import com.example.demo.DTOs.Requests.ProduitDTO;
+import com.example.demo.DTOs.Responses.CommandeResponseDTO;
 import com.example.demo.entities.Client;
 import com.example.demo.entities.Commande;
 import com.example.demo.entities.CommandeProduit;
 import com.example.demo.entities.Produit;
+import com.example.demo.enums.OrderStatus;
 import com.example.demo.exceptions.NotFoundException;
 import com.example.demo.mappers.CommandeMapper;
+import com.example.demo.mappers.ProduitMapper;
 import com.example.demo.repositories.ClientRepository;
 import com.example.demo.repositories.CommandeProduitRepository;
 import com.example.demo.repositories.CommandeRepository;
@@ -15,6 +19,7 @@ import com.example.demo.repositories.ProduitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -22,7 +27,7 @@ public class CommandeService {
     @Autowired
     private CommandeRepository commandeRepository;
     @Autowired
-    private CommandeProduitRepository commandeProduitRepository;
+    private ProduitMapper produitMapper;
     @Autowired
     private CommandeMapper commandeMapper;
     @Autowired
@@ -30,10 +35,10 @@ public class CommandeService {
     @Autowired
     private ClientRepository clientRepository;
 
-    public void creerCommande(CommandeRequestDTO commandeDto){
+    public CommandeResponseDTO creerCommande(CommandeRequestDTO commandeDto){
 
                 Commande commande = commandeMapper.toEntity(commandeDto);
-
+                commande.setOrderStatus(OrderStatus.PENDING);
                 Client client =clientRepository.findById(commandeDto.getClientId()).orElseThrow(
                         ()->new NotFoundException("le client avec l id "+ commandeDto.getClientId()+ "n existe pas")
                 );
@@ -41,17 +46,26 @@ public class CommandeService {
                 for(Long pId: commandeDto.getProduitids()){
                     Produit produit = produitRepository.findById(pId).orElseThrow(
                             ()->new NotFoundException("produit avec l id "+ pId + "n existe pas"));
-
                     CommandeProduit cp = new CommandeProduit();
                     cp.setCommande(commande);
                     cp.setProduit(produit);
-
                     commande.getCommande_produit().add(cp);
+
                 }
-
-
                 commandeRepository.save(commande);
 
-    }
+                CommandeResponseDTO result = commandeMapper.toDTO(commande);
+                result.setClientId(client.getId());
+
+                List<ProduitDTO> produitsDTO = new ArrayList<>();
+
+                for (CommandeProduit cp : commande.getCommande_produit()) {
+                    ProduitDTO pDto = produitMapper.toDTO(cp.getProduit());
+                    produitsDTO.add(pDto);
+                }
+
+                result.setProduits(produitsDTO);
+                return result;
+            }
 
 }
