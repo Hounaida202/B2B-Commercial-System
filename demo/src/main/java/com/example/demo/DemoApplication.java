786a0bdb6c64606191342d8ad1fe6c0ffa6648ed
Hounaida202 <etrichih@gmail.com>
2025-12-01 21:56:31 +0100

    Merges : merger la branche SMR-27-ajout-commande-client avec la main

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import com.example.demo.entities.Client;
+import com.example.demo.entities.User;
+import com.example.demo.seeds.AdminSeeder;
+import com.example.demo.seeds.ClientSeeder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+
+
+    public static void main(String[] args) {
+        var context = SpringApplication.run(DemoApplication.class, args);
+
+        AdminSeeder admin = context.getBean(AdminSeeder.class);
+        ClientSeeder client = context.getBean(ClientSeeder.class);
+
+
+        User adminUser = admin.loadAdmin();
+        Client clientUser = client.loadClient();
+        client.recupererclients();
+
+
+
+    }
+
+}
