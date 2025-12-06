63eae6f7e9916b024c1d141f2195a7fb165efa1c
Hounaida202 <etrichih@gmail.com>
2025-12-06 20:17:51 +0100

    Modifications: regler les attributs en eliminant les attributs unitile

--- demo/src/main/java/com/example/demo/DTOs/Requests/CommandeRequestDTO.java
+++ demo/src/main/java/com/example/demo/DTOs/Requests/CommandeRequestDTO.java
@@ -1,32 +1,15 @@
 package com.example.demo.DTOs.Requests;
 
-import com.example.demo.enums.OrderStatus;
-
+import jakarta.annotation.Nullable;
 import lombok.Data;
 
-import java.time.LocalDate;
 import java.util.List;
-
 @Data
 public class CommandeRequestDTO {
 
-    private List<Long> produitids;
-
-    private Long clientId;
-
-    private LocalDate date;
-
-    private Double sousTotal;
-
-    private Double remise;
-
-    private Double tva;
-
-    private Double total;
-
+    private List<CommandeProduitRequestDTO> produits;
+    @Nullable
     private String codePromo;
 
-    private OrderStatus orderStatus;
-
-    private Double montantRestant;
 }
+
