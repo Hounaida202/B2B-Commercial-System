652262bf9db87549768e62d94ba95d0f201ef70a
Hounaida202 <etrichih@gmail.com>
2025-12-07 20:24:37 +0100

    Creation: creation de la methode jpa pour la recuperation des user selon id

--- demo/src/main/java/com/example/demo/repositories/ClientRepository.java
+++ demo/src/main/java/com/example/demo/repositories/ClientRepository.java
@@ -4,4 +4,6 @@ import com.example.demo.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends JpaRepository<Client,Long> {
+    Client findByUserId(Long userId);
+
 }
