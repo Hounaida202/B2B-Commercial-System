abdbe78c15288c28620c42ea527801562306d920
Hounaida202 <etrichih@gmail.com>
2025-12-07 20:26:14 +0100

    Creation: creation de la methode jpa pour la recuperation des commande par client id , et le counting des commande confirmés d un c lient

--- demo/src/main/java/com/example/demo/repositories/CommandeRepository.java
+++ demo/src/main/java/com/example/demo/repositories/CommandeRepository.java
@@ -1,7 +1,13 @@
 package com.example.demo.repositories;
 
 import com.example.demo.entities.Commande;
+import com.example.demo.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommandeRepository extends JpaRepository<Commande,Long> {
+    int countByClientIdAndOrderStatus(Long clientId, OrderStatus status);
+    List<Commande> findByClientIdAndOrderStatus(Long id , OrderStatus orderStatus);
+
 }
