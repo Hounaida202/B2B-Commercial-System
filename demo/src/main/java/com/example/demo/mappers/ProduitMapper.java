e541d753783831ac395a26b88e65501c5d71f3c7
Hounaida202 <etrichih@gmail.com>
2025-12-07 20:22:26 +0100

    Modifications : l ajout de la methde de mapping vers produit dans produit mapper

--- demo/src/main/java/com/example/demo/mappers/ProduitMapper.java
+++ demo/src/main/java/com/example/demo/mappers/ProduitMapper.java
@@ -1,15 +1,12 @@
 package com.example.demo.mappers;
 
 import com.example.demo.DTOs.Requests.ProduitDTO;
+import com.example.demo.DTOs.Responses.ProduitResponseDTO;
 import com.example.demo.entities.Produit;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel="spring")
 public interface ProduitMapper {
-
-
-
     Produit toEntity(ProduitDTO produitDto);
-    ProduitDTO toDTO(Produit produit);
-
+    ProduitResponseDTO toDTO(Produit produit);
 }
