8564d0c7c77859e0177ffa9340bbee22d1e0996a
Hounaida202 <etrichih@gmail.com>
2025-12-08 20:30:17 +0100

    Créations: l'ajout des endpoint des methodes services creercommande te confirmer commande

--- demo/src/main/java/com/example/demo/controllers/CommandeController.java
+++ demo/src/main/java/com/example/demo/controllers/CommandeController.java
@@ -4,12 +4,10 @@ package com.example.demo.controllers;
 import com.example.demo.DTOs.Requests.CommandeRequestDTO;
 import com.example.demo.DTOs.Responses.CommandeResponseDTO;
 import com.example.demo.services.CommandeService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/commande")
@@ -18,11 +16,18 @@ public class CommandeController {
     private CommandeService commandeService;
 
     @PostMapping("creerCommande")
-    public ResponseEntity<CommandeResponseDTO> creerCommande(@RequestBody CommandeRequestDTO dto){
-        CommandeResponseDTO commandeResponseDTO =commandeService.creerCommande(dto);
+    public ResponseEntity<CommandeResponseDTO> creerCommande(@RequestBody CommandeRequestDTO dto , HttpSession session){
+        CommandeResponseDTO commandeResponseDTO =commandeService.creerCommande(dto ,session);
 
         return ResponseEntity.ok(commandeResponseDTO);
     }
 
+    @PutMapping("/confirmerCommandes/{id}")
+    public ResponseEntity<String> confirmerCommande(@PathVariable Long id) {
+
+        commandeService.confirmerCommande(id);
+
+        return ResponseEntity.status(200).body("succes");
+    }
 
 }
