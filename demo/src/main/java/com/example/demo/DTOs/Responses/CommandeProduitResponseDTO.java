1f90c33f9e92c0225f0c6a65250d13699abf94c4
Hounaida202 <etrichih@gmail.com>
2025-12-08 20:27:24 +0100

    Modifications : modification des attributs de la dto de retourne

--- demo/src/main/java/com/example/demo/DTOs/Responses/CommandeProduitResponseDTO.java
+++ demo/src/main/java/com/example/demo/DTOs/Responses/CommandeProduitResponseDTO.java
@@ -1,4 +1,13 @@
 package com.example.demo.DTOs.Responses;
 
+import com.example.demo.DTOs.Requests.ProduitDTO;
+import com.example.demo.entities.Produit;
+import lombok.Data;
+
+@Data
 public class CommandeProduitResponseDTO {
+    private ProduitResponseDTO produit;
+
+    private int quantite;
 }
+
