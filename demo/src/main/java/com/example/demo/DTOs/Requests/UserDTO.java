506b56577619c51ebc7b6a389b5929a849cb5396
Hounaida202 <etrichih@gmail.com>
2025-12-09 14:31:05 +0100

    Modifications: l ajout de validation regex pour l email

--- demo/src/main/java/com/example/demo/DTOs/Requests/UserDTO.java
+++ demo/src/main/java/com/example/demo/DTOs/Requests/UserDTO.java
@@ -1,9 +1,11 @@
 package com.example.demo.DTOs.Requests;
 
 import com.example.demo.enums.UserRole;
+import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Data;
 
@@ -12,12 +14,15 @@ import lombok.Data;
 public class UserDTO {
 
     @NotBlank(message="email est obligatoire")
-    @Email(message = "format d'email invalide")
+    @Pattern(
+            regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
+            message = "email doit respecter le format standard : exemple@gmail.com"
+    )
     private String username;
 
-    @NotNull(message="role est obligatoire")
+    @Column(nullable = true)
     private UserRole userRole;
 
-    private String password;
+//    private String password;
 
 }
