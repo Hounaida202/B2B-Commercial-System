bfd0ae287c563e669f8b08e4901f417a826505f6
Hounaida202 <etrichih@gmail.com>
2025-12-09 14:15:48 +0100

    Suppression : supprime des importes inutiles

--- demo/src/main/java/com/example/demo/controllers/AuthController.java
+++ demo/src/main/java/com/example/demo/controllers/AuthController.java
@@ -4,7 +4,6 @@ import com.example.demo.DTOs.Requests.AuthRequestDTO;
 import com.example.demo.DTOs.Responses.AuthResponseDTO;
 import com.example.demo.services.AuthService;
 import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +14,6 @@ public class AuthController {
 
       @Autowired
       private AuthService authService;
-
 
       @PostMapping("login")
       public ResponseEntity<?> login( @RequestBody AuthRequestDTO userDTO,
