dd9d8d95b211b9635b31ebde7c59aa224dd739f4
Hounaida202 <etrichih@gmail.com>
2025-12-09 14:30:40 +0100

    Modifications: l ajout de validation regex pattern de code promo

--- demo/src/main/java/com/example/demo/DTOs/Requests/CommandeRequestDTO.java
+++ demo/src/main/java/com/example/demo/DTOs/Requests/CommandeRequestDTO.java
@@ -1,6 +1,7 @@
 package com.example.demo.DTOs.Requests;
 
 import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 import java.util.List;
@@ -9,6 +10,9 @@ public class CommandeRequestDTO {
 
     private List<CommandeProduitRequestDTO> produits;
     @Nullable
+    @Pattern(regexp =
+            "PROMO-[A-Z0-9]{4}",
+    message="ce code promo n est pas valide ")
     private String codePromo;
 
 }
