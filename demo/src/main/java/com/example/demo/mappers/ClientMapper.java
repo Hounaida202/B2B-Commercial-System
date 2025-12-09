cce58e4de13440ece58f37f911bd8af7d5d83c24
Hounaida202 <etrichih@gmail.com>
2025-12-09 14:25:33 +0100

    Modification : ignoration de user dans client mapper

--- demo/src/main/java/com/example/demo/mappers/ClientMapper.java
+++ demo/src/main/java/com/example/demo/mappers/ClientMapper.java
@@ -2,14 +2,18 @@ package com.example.demo.mappers;
 
 import com.example.demo.DTOs.Requests.ClientDTO;
 import com.example.demo.DTOs.Requests.ProduitDTO;
+import com.example.demo.DTOs.Responses.ClientResponseDTO;
 import com.example.demo.entities.Client;
 import com.example.demo.entities.Produit;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel="spring", uses= {UserMapper.class})
 
 public interface ClientMapper {
 
+
+    @Mapping(target = "user", ignore = true)
     Client toEntity(ClientDTO clientDto);
-    ClientDTO toDTO(Client client);
+    ClientResponseDTO toDTO(Client client);
 }
