8b2639a3188ac29668eaed7b37c141b0147d1a56
Hounaida202 <etrichih@gmail.com>
2025-12-09 21:02:02 +0100

    Creation: creation du mapper entre paiement et les dtos

--- demo/src/main/java/com/example/demo/mappers/PaiementMapper.java
+++ demo/src/main/java/com/example/demo/mappers/PaiementMapper.java
@@ -1,4 +1,18 @@
 package com.example.demo.mappers;
 
+import com.example.demo.DTOs.Requests.PaiementRequestDTO;
+import com.example.demo.DTOs.Responses.PaiementResponseDTO;
+import com.example.demo.entities.Paiement;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface PaiementMapper {
+
+    @Mapping(target = "commande", ignore = true)
+    @Mapping(target = "datePaiement", ignore = true)
+    @Mapping(target = "statut", ignore = true)
+    Paiement toEntity(PaiementRequestDTO dto);
+
+    PaiementResponseDTO toDTO(Paiement paiement);
 }
