7d9adf264101b81c149833efafb87f193fe72ced
Hounaida202 <etrichih@gmail.com>
2025-12-09 20:59:41 +0100

    Creation: creation de la classe dto pour les requettes de paiement

--- demo/src/main/java/com/example/demo/DTOs/Requests/PaiementRequestDTO.java
+++ demo/src/main/java/com/example/demo/DTOs/Requests/PaiementRequestDTO.java
@@ -1,4 +1,28 @@
 package com.example.demo.DTOs.Requests;
 
+import com.example.demo.enums.PaymentMethod;
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
 public class PaiementRequestDTO {
+
+    @NotNull(message = "Le montant est obligatoire")
+    @Min(value = 1, message = "Le montant doit être supérieur à 0")
+    private Double montant;
+
+    @NotNull(message = "Le moyen de paiement est obligatoire")
+    private PaymentMethod moyenPaiement;
+
+    @NotNull(message = "La référence est obligatoire")
+    private String reference;
+
+    @Nullable
+    private String banque;
+
+    @Nullable
+    private LocalDate dateEcheance;
 }
