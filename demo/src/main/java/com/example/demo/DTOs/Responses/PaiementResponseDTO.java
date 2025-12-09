25667ad02208a56c4e1b09f0528ac3b05f127d88
Hounaida202 <etrichih@gmail.com>
2025-12-09 21:01:04 +0100

    Création : la classe enum de type de maiement

--- demo/src/main/java/com/example/demo/DTOs/Responses/PaiementResponseDTO.java
+++ demo/src/main/java/com/example/demo/DTOs/Responses/PaiementResponseDTO.java
@@ -1,4 +1,19 @@
 package com.example.demo.DTOs.Responses;
 
+import com.example.demo.enums.PaymentMethod;
+import com.example.demo.enums.PaymentStatus;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
 public class PaiementResponseDTO {
+
+    private Long id;
+    private Double montant;
+    private PaymentMethod moyenPaiement;
+    private String reference;
+    private String banque;
+    private LocalDate dateEcheance;
+    private LocalDate datePaiement;
+    private PaymentStatus statut;
 }
