400f39095dfd56087c7ffd03a8f749ece95831c3
Hounaida202 <etrichih@gmail.com>
2025-12-09 14:29:54 +0100

    Modifications : mettre le customerTier nullable

--- demo/src/main/java/com/example/demo/DTOs/Requests/ClientDTO.java
+++ demo/src/main/java/com/example/demo/DTOs/Requests/ClientDTO.java
@@ -1,6 +1,8 @@
 package com.example.demo.DTOs.Requests;
 
 import com.example.demo.enums.CustomerTier;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.Column;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -12,7 +14,7 @@ public class ClientDTO {
     @NotBlank(message="le nom est obligatoire")
     private String nom;
 
-    @NotNull(message="le role est obligatoire")
+    @Column(nullable = true)
     private CustomerTier customerTier;
 
     @NotNull(message="les infos email et password sont obligatoires")
