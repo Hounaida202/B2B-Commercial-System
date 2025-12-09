925932b0d95ae32d3d0abf22ae1e8ba7880d5fbb
Hounaida202 <etrichih@gmail.com>
2025-12-09 14:19:40 +0100

    Modifcations : modification des methode de service commande

--- demo/src/main/java/com/example/demo/services/CommandeService.java
+++ demo/src/main/java/com/example/demo/services/CommandeService.java
@@ -1,9 +1,8 @@
-package com.example.demo.services;
 
+package com.example.demo.services;
 
 import com.example.demo.DTOs.Requests.CommandeRequestDTO;
 import com.example.demo.DTOs.Requests.CommandeProduitRequestDTO;
-import com.example.demo.DTOs.Requests.ProduitDTO;
 import com.example.demo.DTOs.Responses.CommandeResponseDTO;
 import com.example.demo.entities.Client;
 import com.example.demo.entities.Commande;
@@ -13,8 +12,6 @@ import com.example.demo.enums.CustomerTier;
 import com.example.demo.enums.OrderStatus;
 import com.example.demo.exceptions.NotFoundException;
 import com.example.demo.mappers.CommandeMapper;
-import com.example.demo.mappers.CommandeProduitMapper;
-import com.example.demo.mappers.ProduitMapper;
 import com.example.demo.repositories.ClientRepository;
 import com.example.demo.repositories.CommandeRepository;
 import com.example.demo.repositories.ProduitRepository;
@@ -26,77 +23,84 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CommandeService {
     @Autowired
     private CommandeRepository commandeRepository;
     @Autowired
-    private ProduitMapper produitMapper;
-    @Autowired
     private CommandeMapper commandeMapper;
     @Autowired
     private ProduitRepository produitRepository;
     @Autowired
     private ClientRepository clientRepository;
-
     @Autowired
     private CalculesService calculesService;
-    @Autowired
-    private CommandeProduitMapper commandeProduitMapper;
 
 
-    public CommandeResponseDTO creerCommande(CommandeRequestDTO commandeRequestDTO , HttpSession masession){
-        Commande commande = commandeMapper.toEntity(commandeRequestDTO);
-        commande.setDate(LocalDate.now());
-        Long idClient = (Long) masession.getAttribute("clientID");
+    @Transactional
+    public CommandeResponseDTO creerCommande(CommandeRequestDTO dto, HttpSession session) {
+
+
+        Long idClient = (Long) session.getAttribute("clientID");
         if (idClient == null) {
-            throw new NotFoundException("Client non connecté ou ID manquant dans la session");
+            throw new NotFoundException("Vous devez être connecté ");
         }
-
         Client client = clientRepository.findById(idClient)
                 .orElseThrow(() -> new NotFoundException("Client non trouvé"));
 
+
+
+
+        for (CommandeProduitRequestDTO item : dto.getProduits()) {
+            if (item.getQuantite() <= 0) {
+                throw new IllegalArgumentException("La quantité doit être supérieure à 0");
+            }
+
+            Produit produit = produitRepository.findById(item.getProduitId())
+                    .orElseThrow(() -> new NotFoundException("Produit non trouvé : ID " + item.getProduitId()));
+            if (produit.getStock() < item.getQuantite()) {
+                throw new IllegalArgumentException(
+                        "Stock insuffisant pour " + produit.getNom());
+            }
+        }
+
+
+
+        Commande commande = new Commande();
+        commande.setDate(LocalDate.now());
         commande.setClient(client);
-
         commande.setOrderStatus(OrderStatus.PENDING);
+        commande.setCodePromo(dto.getCodePromo());
+
         List<CommandeProduit> listItems = new ArrayList<>();
-         for(CommandeProduitRequestDTO item : commandeRequestDTO.getProduits()){
+        for (CommandeProduitRequestDTO item : dto.getProduits()) {
+            Produit produit = produitRepository.findById(item.getProduitId()).get();
+            CommandeProduit cp = new CommandeProduit();
+            cp.setQuantite(item.getQuantite());
+            cp.setProduit(produit);
+            cp.setCommande(commande);
+            cp.setPrixUnitaire(produit.getPrix_unitaire());
+            cp.setTotalLigne(produit.getPrix_unitaire() * item.getQuantite());
+            listItems.add(cp);
+        }
+        commande.setCommande_produit(listItems);
 
-             Produit produit = produitRepository.findById(item.getProduitId()).orElseThrow(()->new NotFoundException("produit non trouvé"));
-             CommandeProduit cp = new CommandeProduit();
-             cp.setQuantite(item.getQuantite());
-             cp.setProduit(produit);
-             cp.setCommande(commande);
-             cp.setPrixUnitaire(produit.getPrix_unitaire());
-             cp.setTotalLigne(produit.getPrix_unitaire() * item.getQuantite());
-             listItems.add(cp);
-         }
+        double sousTotal = calculesService.calculerSousTotal(commande);
+        double remise = calculesService.calculerRemise(client, commande);
+        double montantHTApresRemise = calculesService.calculerMontantHTApresRemise(commande, remise);
+        double tva = calculesService.calculerTVA(montantHTApresRemise);
+        double totalTTC = calculesService.calculerTotalTTC(montantHTApresRemise, tva);
 
-         commande.setCommande_produit(listItems);
-         commande.setTotal(calculesService.calculerTotal(commande));
-         commande.setRemise(calculesService.calculerRemise(client , commande));
-         commande.setSousTotal(calculesService.calculerSousTotal(client,commande));
-         commande.setTva(calculesService.calculerTVA(calculesService.calculerTotal(commande)));
-         commande.setMontantRestant(calculesService.calculerTotal(commande));
-         commandeRepository.save(commande);
+        commande.setSousTotal(sousTotal);
+        commande.setRemise(remise);
+        commande.setTva(tva);
+        commande.setTotal(totalTTC);
+        commande.setMontantRestant(totalTTC);
 
+        commandeRepository.save(commande);
 
-          CommandeResponseDTO commandeResponseDTO = commandeMapper.toDTO(commande);
-
-          return commandeResponseDTO;
-    }
-
-
-
-    public Double calculerMontantCume(Commande commande) {
-        Client client = commande.getClient();
-
-        List<Commande> commandesConfirmees = commandeRepository.findByClientIdAndOrderStatus(client.getId(),OrderStatus.CONFIRMED);
-        return commandesConfirmees.stream()
-                .mapToDouble(Commande::getTotal)
-                .sum();
+        return commandeMapper.toDTO(commande);
     }
 
 
@@ -106,48 +110,58 @@ public class CommandeService {
         Commande commande = commandeRepository.findById(commandeId)
                 .orElseThrow(() -> new NotFoundException("Commande introuvable"));
 
+        if (commande.getOrderStatus() != OrderStatus.PENDING) {
+            throw new IllegalArgumentException("Seules les commandes PENDING peuvent être confirmées");
+        }
+
+        for (CommandeProduit cp : commande.getCommande_produit()) {
+            Produit produit = cp.getProduit();
+
+            if (produit.getStock() < cp.getQuantite()) {
+                throw new IllegalArgumentException("Stock insuffisant pour " + produit.getNom());
+            }
+
+            produit.setStock(produit.getStock() - cp.getQuantite());
+            produitRepository.save(produit);
+        }
+
         commande.setOrderStatus(OrderStatus.CONFIRMED);
         commandeRepository.save(commande);
 
         Client client = commande.getClient();
 
-        int confirmedCount = commandeRepository.countByClientIdAndOrderStatus(
+        int nbCommandesConfirmees = commandeRepository.countByClientIdAndOrderStatus(
                 client.getId(),
                 OrderStatus.CONFIRMED
         );
 
-        Double totalCumule = calculerMontantCume(commande);
-        if (totalCumule == null) totalCumule = 0.0;
+        Double montantCumule = calculerMontantCumule(client.getId());
+        if (montantCumule == null) montantCumule = 0.0;
 
-        if (confirmedCount >= 20 || totalCumule >= 15000) {
+        if (nbCommandesConfirmees >= 20 || montantCumule >= 15000) {
             client.setCustomerTier(CustomerTier.PLATINUM);
-        } else if (confirmedCount >= 10 || totalCumule >= 5000) {
+        } else if (nbCommandesConfirmees >= 10 || montantCumule >= 5000) {
             client.setCustomerTier(CustomerTier.GOLD);
-        } else if (confirmedCount >= 3 || totalCumule >= 1000) {
+        } else if (nbCommandesConfirmees >= 3 || montantCumule >= 1000) {
             client.setCustomerTier(CustomerTier.SILVER);
         } else {
             client.setCustomerTier(CustomerTier.BASIC);
         }
 
-        double sousTotal = commande.getSousTotal();
-        double remise = 0.0;
-
-        switch (client.getCustomerTier()) {
-            case SILVER -> { if (sousTotal >= 500) remise = sousTotal * 0.05; }
-            case GOLD -> { if (sousTotal >= 800) remise = sousTotal * 0.10; }
-            case PLATINUM -> { if (sousTotal >= 1200) remise = sousTotal * 0.15; }
-            default -> remise = 0.0;
-        }
-
-        commande.setRemise(remise);
-        commande.setTotal(sousTotal - remise);
         clientRepository.save(client);
-        commandeRepository.save(commande);
+
         return commandeMapper.toDTO(commande);
     }
 
 
+    private Double calculerMontantCumule(Long clientId) {
+        List<Commande> commandesConfirmees = commandeRepository
+                .findByClientIdAndOrderStatus(clientId, OrderStatus.CONFIRMED);
 
+        return commandesConfirmees.stream()
+                .mapToDouble(Commande::getTotal)
+                .sum();
+    }
 
 
 
